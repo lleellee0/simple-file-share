c6792a83c8f87c1d45895a3de0ff43175c83d907
lleellee0 <lleellee013@gmail.com>
2019-03-02 01:37:39 +0900

    업로드시 링크 localhost 형태로 보여주기 완성 // 파일 다운로드 경로 hash로 생성

--- functions.js
+++ functions.js
@@ -16,23 +16,26 @@ button.addEventListener( "click", function( event ) {
 });  
 fileInput.addEventListener( "change", function( event ) {  
     // the_return.innerHTML = this.value;  
+    let file_name = this.files[0].path.split('\\')[this.files[0].path.split('\\').length - 1];
     the_return.innerHTML = this.files[0].path;
 
-    let file_name = this.files[0].path.split('\\')[this.files[0].path.split('\\').length - 1];
 
-    share_link.innerHTML = `Share link : http://localhost:25548/${file_name}`;
+    const crypto = require('crypto');
+    let sha1 = crypto.createHash('sha1');
+    sha1.update(file_name + new Date());
+    let output = sha1.digest('hex').slice(0, 6);
 
+    share_link.innerHTML = `Share link : http://localhost:25548/${output}`;
 
     let express = require('express');
     let app = express();
-    app.get(`/hash`, (req, res) => {
-
-
+    app.get(`/${output}`, (req, res) => {
         let fs = require('fs');
 
-        let stream = fs.createReadStream(this.files[0].path.replace(/\\/g, '\\\\'), { bufferSize: 64 * 1024});
-        res.attachment(file_name);
-        stream.pipe(res);
+        let stream = 
+            fs.createReadStream(this.files[0].path.replace(/\\/g, '\\\\'), { bufferSize: 64 * 1024});  // 스트림 생성
+        res.attachment(file_name);  // 파일 이름 지정
+        stream.pipe(res);   // 스트림으로 응답
     });
 
     app.listen(25548);
